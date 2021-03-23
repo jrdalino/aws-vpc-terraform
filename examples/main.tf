13f9b35853ab411c4b0d11415eb5e2242e9f90be
Jose Dalino <jrdalino@gmail.com>
2021-03-23 17:52:24 +0800

    Initial

--- examples/main.tf
+++ examples/main.tf
@@ -1,18 +1,18 @@
 module "vpc" {
   source = "git::https://github.com/jrdalino/aws-vpc-terraform.git?ref=main"
   # General
-  aws_region              = var.aws_region
-  aws_account             = var.aws_account
-  aws_role                = var.aws_role
+  aws_region  = var.aws_region
+  aws_account = var.aws_account
+  aws_role    = var.aws_role
 
   # Tagging
-  namespace               = var.namespace
-  bounded_context         = var.bounded_context
-  environment             = var.environment
+  namespace       = var.namespace
+  bounded_context = var.bounded_context
+  environment     = var.environment
 
   # Network - General
-  subnet_count            = var.subnet_count
-  aws_eks_cluster_name    = var.aws_eks_cluster_name
+  subnet_count         = var.subnet_count
+  aws_eks_cluster_name = var.aws_eks_cluster_name
 
   # VPC
   aws_vpc_name = var.aws_vpc_name
